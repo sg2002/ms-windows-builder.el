7b3900277907eb0148c89a56eccd5b58a656b67c
sg2002 <sg2002@gmx.com>
2021-12-14 22:12:10 +0300

    Updated Texinfo for Mingw to 6.8.

--- ms-windows-builder-config.el
+++ ms-windows-builder-config.el
@@ -152,7 +152,7 @@ Currently it only allows to limit use of specific arguments by toolchains."
     ("https://sourceforge.net/projects/ezwinports/files/"
      ("automake-1.11.6-msys-bin.zip"
       "autoconf-2.65-msys-bin.zip"
-      "texinfo-6.7-w32-bin.zip"))))
+      "texinfo-6.8-w32-bin.zip"))))
 
 (defvar mwb-mingw-packages
   '(("https://sourceforge.net/projects/mingw/files/MinGW/Base/"
